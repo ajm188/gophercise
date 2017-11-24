2f542185fb1e4f11339156e12aceda7f09f1be2d
Andrew Mason <andrew@fixedpoint.xyz>
2017-11-24 06:18:51 -0500

    Move link-parsing logic to a subpackage

--- link/main.go
+++ link/main.go
@@ -2,65 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 )
 
 import (
-	"golang.org/x/net/html"
+	"github.com/ajm188/gophercise/link/link"
 )
-
-type Link struct {
-	Href string
-	Text string
-}
-
-func findLinks(r io.Reader) []Link {
-	tokenizer := html.NewTokenizer(r)
-	links := make([]Link, 0)
-	for {
-		switch tokenizer.Next() {
-		case html.ErrorToken:
-			return links
-		case html.StartTagToken:
-			tag, attrs := tokenizer.TagName()
-			if string(tag) == "a" && attrs {
-				href := findHref(tokenizer)
-				text := findText(tokenizer)
-				links = append(links, Link{href, text})
-			}
-		}
-	}
-	return links
-}
-
-func findText(tokenizer *html.Tokenizer) string {
-	fullText := ""
-	for {
-		switch tokenizer.Next() {
-		case html.TextToken:
-			fullText += string(tokenizer.Text())
-		case html.EndTagToken:
-			tag, _ := tokenizer.TagName()
-			if string(tag) == "a" {
-				return fullText
-			}
-		}
-	}
-	return fullText
-}
-
-func findHref(tokenizer *html.Tokenizer) string {
-	for {
-		name, val, hasMore := tokenizer.TagAttr()
-		if string(name) == "href" {
-			return string(val)
-		}
-		if !hasMore {
-			return ""
-		}
-	}
-}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -72,5 +19,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(findLinks(file))
+	fmt.Println(link.FindLinks(file))
 }
